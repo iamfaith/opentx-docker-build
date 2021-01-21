e64279a9e2ac96ad8c2f5073d989a2deb3db4493
Nicola Guerrera <guerrera.nicola@gmail.com>
2021-01-21 09:22:45 +0100

    Fixed bug

--- build-fw.py
+++ build-fw.py
@@ -153,14 +153,14 @@ elif board_name == "tx16s":
     firmware_options = options_radiomaster_tx16s
     maxsize = 2 * 1024 * 1024
 elif board_name == "t18":
-    cmake_options["PCB"] = "X10"
-    cmake_options["PCBREV"] = "T18"
+    extra_options["PCB"] = "X10"
+    extra_options["PCBREV"] = "T18"
     firmware_options = options_jumper_t18
     maxsize = 2 * 1024 * 1024
 elif board_name == "tlite":
-    cmake_options["PCB"] = "X7"
-    cmake_options["PCBREV"] = "TLITE"
-    firmware_options = options_jumper_t18
+    extra_options["PCB"] = "X7"
+    extra_options["PCBREV"] = "TLITE"
+    firmware_options = options_taranis_xlite
     maxsize = 2 * 1024 * 1024
 else:
     firmware_options = generic_default_options
